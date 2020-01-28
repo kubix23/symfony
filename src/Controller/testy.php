50a756fe9ea87b1064fb0df3520972e4a39fd411
unknown <kubix2770@gmail.com>
2020-01-28 22:22:11 +0100

    cwiczenie twiga(tłumacz tłumaczy jako gałązkę)

--- src/Controller/testy.php
+++ src/Controller/testy.php
@@ -1,16 +1,26 @@
 <?php
 
 namespace App\Controller;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class testy
+class testy extends AbstractController
 {
     /**
      * @Route("/arts/{art}")
      */
-    public function homepage($art)
+    public function functest($art)
     {
-        return new Response('OMG! My first page already ' . $art);
+        $comments = [
+            'I ate a normal rock once. It did NOT taste like bacon!',
+            'Woohoo! I\'m going on an all-asteroid diet!',
+            'I like bacon too! Buy some from my site! bakinsomebacon.com',
+            'Random comment',
+        ];
+        return $this -> render("article/functest.html.twig",[
+            "title" => ucwords(str_replace("-", " ",$art )),
+            "comments" => $comments,
+        ]);
     }
 }
